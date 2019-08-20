185ebdbc7d8a47a53cd6f549aa39f2a10283e2b7
JacobMillner <jacob.millner@outlook.com>
2019-08-20 12:29:31 -0400

    custom css cards for users

--- frontend/src/Components/Users/Users.js
+++ frontend/src/Components/Users/Users.js
@@ -21,24 +21,22 @@ class Users extends Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    {this.state.users === null && <p>Loading users...</p>}
-                    {
-                        this.state.users && this.state.users.map(user => (
-                            <div key={user.username} className="col-sm-12 col-md-4 col-lg-3">
-                                <Link to={`/users/${user.username}`}>
-                                    <div className="card text-white bg-success mb-3">
-                                        <div className="card-header">UserName: {user.username}</div>
-                                        <div className="card-body">
-                                            <h4 className="card-title">{user.email}</h4>
-                                        </div>
+            <div className="flex-container">
+                {this.state.users === null && <p>Loading users...</p>}
+                {
+                    this.state.users && this.state.users.map(user => (
+                        <div key={user.username} className="eggplant">
+                            <Link to={`/users/${user.username}`} className="purp">
+                                <div className="flex-card">
+                                    <div className="card-header">UserName: {user.username}</div>
+                                    <div className="card-body">
+                                        <h4 className="card-title">{user.email}</h4>
                                     </div>
-                                </Link>
-                            </div>
-                        ))
-                    }
-                </div>
+                                </div>
+                            </Link>
+                        </div>
+                    ))
+                }
             </div>
         )
     }
