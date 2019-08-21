5c173febe1f644d24f839c784834869227bd6f57
JacobMillner <jacob.millner@outlook.com>
2019-08-21 14:26:38 -0400

    getting started on front end authentication services

---
+++ frontend/src/Services/Authentication.js
@@ -0,0 +1,61 @@
+import decode from 'jwt-decode';
+import axios from 'axios';
+
+export default class AuthService {
+
+    constructor(domain) {
+        // only used in production mode
+        this.domain = domain || 'http://localhost:8888';
+    }
+
+    login = (username, password) => {
+
+        // Get a token from api server using the fetch api
+        return axios.post(this.domain + '/user', {
+            username: username,
+            password: password
+        }).then(res => {
+
+            this.setToken(res.token) // Setting the token in localStorage
+            return Promise.resolve(res);
+        })
+    }
+
+    loggedIn = () => {
+        // Checks if there is a saved token and it's still valid
+        const token = this.getToken() // Getting token from localstorage
+
+        //The double exclamation is a way to cast the variable to a boolean, allowing you to easily check if the token exusts. 
+        return !!token && !this.isTokenExpired(token) // handwaiving here
+    }
+
+    isTokenExpired = (token) => {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) { // Checking if token is expired.
+                return true;
+            }
+            else
+                return false;
+        }
+        catch (err) {
+            console.log("expired check failed! Line 42: AuthService.js");
+            return false;
+        }
+    }
+
+    setToken = (idToken) => {
+        // Saves user token to localStorage
+        localStorage.setItem('id_token', idToken)
+    }
+
+    getToken = () => {
+        // Retrieves the user token from localStorage
+        return localStorage.getItem('id_token')
+    }
+
+    logout = () => {
+        // Clear user token and profile data from localStorage
+        localStorage.removeItem('id_token');
+    }
+}
