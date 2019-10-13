fee27af8fccf147a440af7f0d07c63943d752831
JacobMillner <jacob.millner@outlook.com>
2019-10-13 13:29:09 -0400

    WIP - redoing UI with antd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import NewMeal from './Components/Meals/NewMeal';
 import Meal from './Components/Meals/Meal';
 import Meals from './Components/Meals/Meals';
 import rootReducer from './Reducers/RootRecuder';
+import { Layout, Breadcrumb } from 'antd';
 
 const store = createStore(
   rootReducer,
@@ -24,19 +25,34 @@ const store = createStore(
 );
 
 function App() {
+
+  const { Header, Content, Footer} = Layout;
   return (
     <Provider store={store}>
       <div>
-        <MainNavBar />
-        <FlashMessagesList />
-        <Route exact path='/' component={Splash} />
-        <Route exact path='/login/' component={Login} />
-        <Route exact path='/signup/' component={Signup} />
-        <Route exact path='/users/' component={Users} />
-        <Route exact path='/user/:id' component={User} />
-        <Route exact path='/meals/' component={Meals} />
-        <Route exact path='/meals/:id(\d+)' component={Meal} />
-        <Route exact path='/meals/new' component={NewMeal} />
+        <Layout style={{ minHeight: '100vh' }}>
+          <MainNavBar />
+          <FlashMessagesList />
+          <Layout>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          </Layout>
+          <Route exact path='/' component={Splash} />
+          <Route exact path='/login/' component={Login} />
+          <Route exact path='/signup/' component={Signup} />
+          <Route exact path='/users/' component={Users} />
+          <Route exact path='/user/:id' component={User} />
+          <Route exact path='/meals/' component={Meals} />
+          <Route exact path='/meals/:id(\d+)' component={Meal} />
+          <Route exact path='/meals/new' component={NewMeal} />
+        </Layout>
       </div>
     </Provider >
   );
--- frontend/src/Components/MainNavBar/MainNavBar.js
+++ frontend/src/Components/MainNavBar/MainNavBar.js
@@ -1,19 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Menu, Icon, Layout } from 'antd';
 import './MainNavBar.css';
 
 
 function MainNavBar() {
+
+    const [collapsed, setcollapsed] = useState(false);
+
+    const { SubMenu } = Menu;
+
+    const { Sider } = Layout;
+    
+    const toggleCollapsed = () => {
+        setcollapsed(!collapsed);
+    };
+
     return (
-        <nav>
-            <ul className="menu">
-                <li className="logo"><Link to="/">Pika Prep</Link></li>
-                <li className="item"><Link to="/meals/">Meals</Link></li>
-                <li className="item"><Link to="/users/">Users</Link></li>
-                <li className="item button"><Link to="/login">Log In</Link></li>
-                <li className="item button secondary"><Link to="/signup">Sign Up</Link></li>
-            </ul>
-        </nav>
+        <Sider collapsible collapsed={collapsed} onCollapse={toggleCollapsed}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="user" />
+                  <span>User</span>
+                </span>
+              }
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Team</span>
+                </span>
+              }
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span>File</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        // <nav>
+        //     <ul className="menu">
+        //         <li className="logo"><Link to="/">Pika Prep</Link></li>
+        //         <li className="item"><Link to="/meals/">Meals</Link></li>
+        //         <li className="item"><Link to="/users/">Users</Link></li>
+        //         <li className="item button"><Link to="/login">Log In</Link></li>
+        //         <li className="item button secondary"><Link to="/signup">Sign Up</Link></li>
+        //     </ul>
+        // </nav>
     );
 }
 
