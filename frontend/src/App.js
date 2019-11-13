42407389879c309cd3ae1965c06d7589d71b29a2
JacobMillner <jacob.millner@outlook.com>
2019-11-13 05:58:54 -0500

    progress on antd reskin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,22 +36,17 @@ function App() {
           <Layout>
           <Header style={{ background: '#fff', padding: 0 }} />
           <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
+            <Route exact path='/' component={Splash} />
+            <Route exact path='/login/' component={Login} />
+            <Route exact path='/signup/' component={Signup} />
+            <Route exact path='/users/' component={Users} />
+            <Route exact path='/user/:id' component={User} />
+            <Route exact path='/meals/' component={Meals} />
+            <Route exact path='/meals/:id(\d+)' component={Meal} />
+            <Route exact path='/meals/new' component={NewMeal} />
           </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          <Footer style={{ textAlign: 'center' }}>Jacob Millner ©2019</Footer>
           </Layout>
-          <Route exact path='/' component={Splash} />
-          <Route exact path='/login/' component={Login} />
-          <Route exact path='/signup/' component={Signup} />
-          <Route exact path='/users/' component={Users} />
-          <Route exact path='/user/:id' component={User} />
-          <Route exact path='/meals/' component={Meals} />
-          <Route exact path='/meals/:id(\d+)' component={Meal} />
-          <Route exact path='/meals/new' component={NewMeal} />
         </Layout>
       </div>
     </Provider >
--- frontend/src/Components/MainNavBar/MainNavBar.js
+++ frontend/src/Components/MainNavBar/MainNavBar.js
@@ -6,69 +6,69 @@ import './MainNavBar.css';
 
 function MainNavBar() {
 
-    const [collapsed, setcollapsed] = useState(false);
+  const [collapsed, setcollapsed] = useState(false);
 
-    const { SubMenu } = Menu;
+  const { SubMenu } = Menu;
 
-    const { Sider } = Layout;
-    
-    const toggleCollapsed = () => {
-        setcollapsed(!collapsed);
-    };
+  const { Sider } = Layout;
 
-    return (
-        <Sider collapsible collapsed={collapsed} onCollapse={toggleCollapsed}>
-          <div className="logo" />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1">
-              <Icon type="pie-chart" />
-              <span>Option 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="desktop" />
-              <span>Option 2</span>
-            </Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <Icon type="user" />
-                  <span>User</span>
-                </span>
-              }
-            >
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={
-                <span>
-                  <Icon type="team" />
-                  <span>Team</span>
-                </span>
-              }
-            >
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9">
-              <Icon type="file" />
-              <span>File</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        // <nav>
-        //     <ul className="menu">
-        //         <li className="logo"><Link to="/">Pika Prep</Link></li>
-        //         <li className="item"><Link to="/meals/">Meals</Link></li>
-        //         <li className="item"><Link to="/users/">Users</Link></li>
-        //         <li className="item button"><Link to="/login">Log In</Link></li>
-        //         <li className="item button secondary"><Link to="/signup">Sign Up</Link></li>
-        //     </ul>
-        // </nav>
-    );
+  const toggleCollapsed = () => {
+    setcollapsed(!collapsed);
+  };
+
+  return (
+    <Sider collapsible collapsed={collapsed} onCollapse={toggleCollapsed}>
+      <div className="logo" />
+      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+        <Menu.Item key="1">
+          <Link to="/">
+            <Icon type="home" />
+            <span>Home</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Link to="/signup">
+            <Icon type="user-add" />
+            <span>Signup</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Link to="/login">
+            <Icon type="login" />
+            <span>Signin</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <Link to="/meals">
+            <Icon type="fire" />
+            <span>Meals</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="5">
+          <Link to="/users">
+            <Icon type="user" />
+            <span>Users</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="6">
+          <Link to="/logout">
+            <Icon type="logout" />
+            <span>logout</span>
+          </Link>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+
+    // <nav>
+    //     <ul className="menu">
+    //         <li className="logo"><Link to="/">Pika Prep</Link></li>
+    //         <li className="item"><Link to="/meals/">Meals</Link></li>
+    //         <li className="item"><Link to="/users/">Users</Link></li>
+    //         <li className="item button"><Link to="/login">Log In</Link></li>
+    //         <li className="item button secondary"><Link to="/signup">Sign Up</Link></li>
+    //     </ul>
+    // </nav>
+  );
 }
 
 export default MainNavBar;
