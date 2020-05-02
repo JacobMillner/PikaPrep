9de9b3266224e86971a0785a429d13ec20ff73d4
Jacob Millner <jacob.millner@outlook.com>
2020-05-02 14:00:56 -0400

    Gathering meal data on backend.

--- backend/src/handlers/mealEntries.py
+++ backend/src/handlers/mealEntries.py
@@ -4,6 +4,7 @@ from auth import has_auth
 from tornado_sqlalchemy import SessionMixin, as_future
 from .base import BaseHandler
 from entities.mealEntries import MealEntry, MealEntrySchema
+from entities.meal import Meal, MealSchema
 from loguru import logger
 
 
@@ -40,8 +41,18 @@ class MealEntriesHandler(SessionMixin, BaseHandler):
             mealEntry_objects = await as_future((session.query(MealEntry).filter(MealEntry.created_by == uid).all))
             # transforming into JSON-serializable objects
             if mealEntry_objects is not None:
+                resp_data = []
                 schema = MealEntrySchema(many=True)
                 mealEntries = schema.dump(mealEntry_objects)
+                for entry_obj in mealEntry_objects:
+                    meal_object = await as_future((session.query(Meal).filter(Meal.id == entry_obj.meal_id).first))
+                    entrySchema = MealEntrySchema()
+                    mealSchema = MealSchema()
+                    meal = mealSchema.dump(meal_object)
+                    entry = entrySchema.dump(entry_obj)
+                    entry_and_meal = {"entry": entry.data, "meal": meal.data}
+                    resp_data.append(entry_and_meal)
+                    # todo: respond with resp_data
                 self.respond(mealEntries.data, "Success", 200)
             else:
                 self.respond(msg="No User with that id!")
