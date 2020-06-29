72693b360d6162fa24c0c6630c62e9dd35ce945f
Jacob Millner <jacob.millner@outlook.com>
2020-06-29 15:21:11 -0400

    finishing typescript implementation, converting logout.js

--- frontend/src/Components/Logout/Logout.tsx
+++ frontend/src/Components/Logout/Logout.tsx
@@ -1,11 +1,12 @@
-import React, { useContext, useEffect } from "react";
+import * as React from "react";
+import { useContext, useEffect } from "react";
 import { message } from "antd";
 import { authService } from "../../Services/AuthService";
 import { LoggedInContext } from "../../Context/is-logged-in-context";
 
 const Logout = () => {
   // hacky way to only get the setter
-  const [, setLoggedIn] = useContext(LoggedInContext);
+  const [, setLoggedIn] = useContext<any>(LoggedInContext);
   // just logout the user and push back to home
   useEffect(() => {
     authService.logout();
