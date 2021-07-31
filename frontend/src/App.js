fa430d59a44cc860acc25b8f898de5bcf4e898d8
jmillner <jacob.millner@outlook.com>
2021-07-31 10:01:54 -0400

    remove redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { createStore, compose, applyMiddleware } from 'redux';
 import MainNavBar from './Components/MainNavBar/MainNavBar';
 import Splash from './Components/Splash/Splash';
 import Users from './Components/Users/Users';
@@ -18,44 +15,34 @@ import PlanMeal from './Components/PlanMeal/PlanMeal';
 import { Layout } from 'antd';
 import { LoggedInProvider } from './Context/is-logged-in-context';
 
-const store = createStore(compose(applyMiddleware(thunk)));
-
 function App() {
   const { Header, Content, Footer } = Layout;
   return (
-    <Provider store={store}>
-      <LoggedInProvider>
-        <div>
-          <Layout style={{ minHeight: '100vh' }}>
-            <MainNavBar />
-            <Layout>
-              <Header style={{ background: '#fff', padding: 0 }} />
-              <Content style={{ margin: '0 0px' }}>
-                <Route exact path="/" component={Splash} />
-                <Route exact path="/login/" component={Login} />
-                <Route exact path="/logout/" component={Logout} />
-                <Route exact path="/signup/" component={Signup} />
-                <Route exact path="/users/" component={Users} />
-                <Route exact path="/user/:id" component={User} />
-                <Route exact path="/meals/" component={Meals} />
-                <Route exact path="/meals/:id(\d+)" component={Meal} />
-                <Route exact path="/planmeal/:id(\d+)" component={PlanMeal} />
-                <Route exact path="/meals/new" component={NewMeal} />
-                <Route exact path="/calendar/" component={PrepCalendar} />
-                <Route
-                  exact
-                  path="/calendar/:id(\d+)"
-                  component={PrepCalendar}
-                />
-              </Content>
-              <Footer style={{ textAlign: 'center' }}>
-                Jacob Millner ©2020
-              </Footer>
-            </Layout>
+    <LoggedInProvider>
+      <div>
+        <Layout style={{ minHeight: '100vh' }}>
+          <MainNavBar />
+          <Layout>
+            <Header style={{ background: '#fff', padding: 0 }} />
+            <Content style={{ margin: '0 0px' }}>
+              <Route exact path="/" component={Splash} />
+              <Route exact path="/login/" component={Login} />
+              <Route exact path="/logout/" component={Logout} />
+              <Route exact path="/signup/" component={Signup} />
+              <Route exact path="/users/" component={Users} />
+              <Route exact path="/user/:id" component={User} />
+              <Route exact path="/meals/" component={Meals} />
+              <Route exact path="/meals/:id(\d+)" component={Meal} />
+              <Route exact path="/planmeal/:id(\d+)" component={PlanMeal} />
+              <Route exact path="/meals/new" component={NewMeal} />
+              <Route exact path="/calendar/" component={PrepCalendar} />
+              <Route exact path="/calendar/:id(\d+)" component={PrepCalendar} />
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Jacob Millner ©2020</Footer>
           </Layout>
-        </div>
-      </LoggedInProvider>
-    </Provider>
+        </Layout>
+      </div>
+    </LoggedInProvider>
   );
 }
 
